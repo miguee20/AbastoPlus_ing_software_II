d2021d9d06c8782392be496fbd34e5b844a9d3f9
miguee20 <migueelsaalguero@gmail.com>
2026-02-26 09:33:27 -0600

    main hotfix

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
-import { Product } from "./catalog/product/domain/product.js";
 import { MongoProductRepository } from "./catalog/product/infrastructure/MongoProductRepository.js";
+import { SaveProduct } from "./catalog/product/application/use-cases/save-product.js";
 
 const MONGO_URI = "mongodb://127.0.0.1:27017/abasto_plus";
 
@@ -10,29 +10,32 @@ const runTests = async (): Promise<void> => {
         await mongoose.connect(MONGO_URI);
         console.log("Conectado a la base de datos");
 
-        console.log("\n--- INICIANDO PRUEBA DE DOMINIO E INFRAESTRUCTURA ---");
-        const validProductId: string = "550e8400-e29b-41d4-a116-446655440000";
-        const miProducto = Product.build(validProductId, "Café de Altura Huehuetenango", "lb");
-        const rawPresentations = [
-            {
-                id: "123e4567-e89b-12d3-a456-426614174001",
-                name: "Bolsa Pequeña de 5 Libras",
-                type: "bag", 
-                netQuantity: 1,
-                UnitOfMeasure: "lb"
-            }
-        ];
-        miProducto.loadPresentations(rawPresentations);
-        console.log("Entidad de dominio construida y validada.");
+        console.log("\n--- INICIANDO PRUEBA CON CASO DE USO ---");
         const repository = new MongoProductRepository();
-        await repository.save(miProducto);
+        const saveProduct = new SaveProduct(repository);
+
+        
+        await saveProduct.execute({
+            id: "550e8401-e29b-41d4-a116-446655440000",
+            name: "Atol de masa Xela",
+            baseUnit: "lt",
+            presentations: [
+                {
+                    id: "123e4267-e89b-12d3-a456-426614174001",
+                    name: "Jarra Pequeña de 1 litro",
+                    type: "jar", 
+                    netQuantity: 1,
+                    UnitOfMeasure: "lt"
+                }
+            ]
+        });
 
     } catch (error: any) {
-        console.log("\n Error capturado:");
+        console.log("\nError capturado por el dominio:");
         console.log(error.message);
     } finally {
         await mongoose.connection.close();
-        console.log("🔌 Conexión cerrada.");
+        console.log("Conexión cerrada.");
     }
 };
 
